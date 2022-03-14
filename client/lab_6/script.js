cfb12825b8aac45dac578e2fef4424c5755e3695
maximgalkin <maxim.a.galkin@gmail.com>
2022-03-13 21:43:26 -0400

    eh Lab 6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -19,6 +19,16 @@ function dataHandler(array) {
   return restos;
 }
 
+function createHtmlList(collection) {
+  console.log(collection);
+  const targetList = doocument.querySelector('.resto-list');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const injectThisItem = `<li>${item.name}</li>`;
+    targetList.innerHTML += injectThisItem;
+  });
+}
+
 async function mainEvent() { // the async keyword means we can make API requests
   console.log('script loaded');
   const form = document.querySelector('.main_form');
@@ -27,7 +37,7 @@ async function mainEvent() { // the async keyword means we can make API requests
   const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
   console.log(arrayFromJson);
-  if (arrayFromJson.data.length > 0) {
+  if (arrayFromJson.length > 0) {
     submit.style.display = 'block';
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
@@ -36,6 +46,7 @@ async function mainEvent() { // the async keyword means we can make API requests
       // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
       const restoArray = dataHandler(arrayFromJson.data);
+      createHtmlList(restoArray);
     });
   }
 }
