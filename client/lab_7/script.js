37faa7b15db5ec4b7d1913d38ddbbb56afd79c24
maximgalkin <maxim.a.galkin@gmail.com>
2022-04-26 15:15:43 -0400

    Quick lb 7 Heroku test

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,56 +1,70 @@
 function getRandomIntInclusive(min, max) {
-    const newMin = Math.ceil(min);
-    const newMax = Math.floor(max);
-    return Math.floor(Math.random() * (newMax - newMin + 1) + newMin);
-  }
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin);
+}
   
   
-  function dataHandler(array) {
-    console.log('hello world');
-    const target = document.querySelector('#resto-list');
-    target.innerHTML = '';
-    console.log(target);
-    const range = [...Array(15).keys()];
-    const restos = range.map((m) => {
-      const index = getRandomIntInclusive(0, array.length);
-      return array[index];
+function dataHandler(dataArray) {
+  console.log('fired dataHandler');
+  console.table(dataArray);
+  // console.log(target);
+  const range = [...Array(15).keys()];
+  const listItems = range.map((item, index) =>{
+    const restNum = getRandomIntInclusive(0, dataArray.length - 1);
+    return dataArray[restNum];
+  });
+
+  console.log(listItems);
+  return listItems;
+  //new code test:
+ // range.forEach((item) => {
+ //   console.log('range item', item);
+ // });
+  //const restos = range.map((m) => {
+  //  const index = getRandomIntInclusive(0, dataArray.length);
+  //  return dataArray[index];
+  // });
+  //console.log(restos);
+  //return restos;
+}
+  
+function createHtmlList(collection) {
+  console.log(collection);
+  const targetList = doocument.querySelector('.resto-list');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const injectThisItem = `<li>${item.name}</li>`;
+    targetList.innerHTML += injectThisItem;
+  });
+}
+  
+async function mainEvent() { // the async keyword means we can make API requests
+  console.log('script loaded');
+  const form = document.querySelector('.main_form');
+  const submit = document.querySelector('.submit_button');
+  submit.style.display = 'none';
+
+  const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  console.log(arrayFromJson);
+  console.log('i break here');
+
+
+  if (arrayFromJson.length > 0) {
+    submit.style.display = 'block';
+    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      console.log('form submission'); // this is substituting for a "breakpoint"
+      // console.table(arrayFromJson.data); // this is called "dot notation"
+      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+      // it contains all 1,000 records we need
+      const restoArray = dataHandler(arrayFromJson.data);
+      createHtmlList(restoArray);
     });
-    console.log(restos);
-    return restos;
   }
+}
   
-  function createHtmlList(collection) {
-    console.log(collection);
-    const targetList = doocument.querySelector('.resto-list');
-    targetList.innerHTML = '';
-    collection.forEach((item) => {
-      const injectThisItem = `<li>${item.name}</li>`;
-      targetList.innerHTML += injectThisItem;
-    });
-  }
-  
-  async function mainEvent() { // the async keyword means we can make API requests
-    console.log('script loaded');
-    const form = document.querySelector('.main_form');
-    const submit = document.querySelector('.submit_button');
-    submit.style.display = 'none';
-    const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.log(arrayFromJson);
-    if (arrayFromJson.length > 0) {
-      submit.style.display = 'block';
-      form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-        submitEvent.preventDefault(); // This prevents your page from refreshing!
-        console.log('form submission'); // this is substituting for a "breakpoint"
-        // console.table(arrayFromJson.data); // this is called "dot notation"
-        // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-        // it contains all 1,000 records we need
-        const restoArray = dataHandler(arrayFromJson.data);
-        createHtmlList(restoArray);
-      });
-    }
-  }
-  
-  // this actually runs first! It's calling the function above
-  document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+// this actually runs first! It's calling the function above
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
   
